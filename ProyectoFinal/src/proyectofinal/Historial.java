d4e14fa71f45535567d21f2a79378b039ecc45de
Andrés Ante Vargas <andrzante.espol@gmail.com>
2017-08-15 15:19:49 -0500

    Se creo la ventana fondo de historial y el  correspondiente label mas el archivo historial.txt

--- ProyectoFinal/src/proyectofinal/Historial.java
+++ ProyectoFinal/src/proyectofinal/Historial.java
@@ -5,10 +5,68 @@
  */
 package proyectofinal;
 
+import java.io.File;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
 /**
  *
  * @author HP ELECTRONIC
  */
 public class Historial {
+    private Pane windowHist;
+    private File file;
+    private ImageView ivHist;
+    private Image imageHist;
+    private TitulosLabel tl;
+    private Text text ;
+
+    public Historial() {
+        this.getWindowHist();
+         
+        
+        //Agregar al pane
+//        
+
+    }
+
+    public Pane getWindowHist() {
+        windowHist = new Pane();
+        try{
+            imageHist= new Image("pirate_av4j.jpg");
+        }catch(Exception e){System.out.println("No lee imagen ");}
+        ivHist = new ImageView(imageHist);
+        ivHist.setFitWidth(820);
+        ivHist.setFitHeight(570);
+        windowHist.getChildren().addAll(ivHist,this.getText());
+
+        return windowHist;
+    }
+
+    public ImageView getIvHist() {
+        return ivHist;
+    }
+
+    public File getFile() {
+        
+        return file;
+    }
     
+    public void MostrarDatos(){
+       //TitulosLabel tl=new TitulosLabel("HISTORIAL", estilo, 0, 0, 0, 0, Color.CORALColor.CORAL); 
+      
+       TextField textField = new TextField();
+       textField.setText("Neon Sign");
+       text.textProperty().bind(textField.textProperty());
+    }
+
+    public Text getText() {
+        return text;
+    }
+
+
 }
